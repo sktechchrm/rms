0a63915e9bda4573711fa08adc0b0c2d30375a7f
saifulislam-74@dipti.com.bd <saifulislam-74@dipti.com.bd>
2026-06-26 23:09:30 +0600

    Again update three files in vew version

---
+++ src/vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SHEETS_URL?:    string;
+  readonly VITE_SHEETS_KEY?:    string;
+  readonly VITE_GRIEVANCE_URL?: string;
+  readonly VITE_DB_ADAPTER?:    string;
+  readonly VITE_MYSQL_API_URL?: string;
+  readonly VITE_MYSQL_API_KEY?: string;
+  readonly DEV:                 boolean;
+  readonly PROD:                boolean;
+  readonly MODE:                string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
